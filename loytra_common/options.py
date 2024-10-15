1a8ba31770ca40073616137267dbbd33c0c348ec
schef <zvanstefan@gmail.com>
2024-10-15 13:49:22 +0200

    updated options

--- loytra_common/options.py
+++ loytra_common/options.py
@@ -1,4 +1,10 @@
+import os
+
 class Options:
     verbose: bool = False
 
 options = Options()
+
+loytra_verbose = os.environ.get("LOYTRA_VERBOSE", "0")
+if loytra_verbose == "1":
+    options.verbose = True
